e864a9957ff030c678039a1f2f4363d787125d5b
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2018-02-01 08:56:38 +0100

    Add network anonymization by using Shalon

--- Todo/TaskModel.swift
+++ Todo/TaskModel.swift
@@ -70,7 +70,7 @@ class TaskModel {
 	let persona = Persona(uniqueName: "primaryUser")
 
 	private let context = SecureKeyValueStorage.Context("TODOLIST")!
-	private let privacyService = PrivacyService(baseUrl: URL(string: "https://privacyservice.test:8080")!)
+	private let privacyService = PrivacyService(baseUrl: URL(string: "httpss://shalon1.jondonym.net:443/apppets.aot.tu-berlin.de:2235")!)
 	private let storage: SecureKeyValueStorage
 	private var taskIds: Set<TaskId> = []
 
